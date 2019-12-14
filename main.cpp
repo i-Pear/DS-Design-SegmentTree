60fbc1fa64299c5137085361bd1670f5fab5865a
iPear <i.pear@outlook.com>
2019-12-14 21:12:27 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class SegmentTree3DNode{
+public:
+    int xLeft,xRight;
+    int yLeft,yRight;
+    int xMid,yMid;
+
+};
+
+template <typename T>
+class SegmentTree3D{
+public:
+    int xLength;
+    int yLength;
+    T** data;
+    SegmentTree3D(int xLength,int yLength,T** source){
+
+    }
+};
+
 
 int main(){
     std::cout<<"Hello, World!"<<std::endl;
