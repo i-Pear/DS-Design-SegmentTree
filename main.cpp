9501b60d6fbbedd3c936cab687cde61cb7512774
iPear <i.pear@outlook.com>
2019-12-14 21:33:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
+class Segment3D{
+public:
+    int xL,xR,yL,yR,zL,zR;
+
+    Segment3D(int xL,int xR,int yL,int yR,int zL,int zR):
+    xL(xL),xR(xR),yL(yL),yR(yR),zL(zL),zR(zR){}
+    int getXLeft(){ return xL;}
+    int getXRight(){ return xR;}
+    int getYLeft(){ return yL;}
+    int getYRight(){ return yR;}
+    int getZLeft(){ return zL;}
+    int getZRight(){ return zR;}
+};
+
+template<typename T>
 class SegmentTree3DNode{
 public:
     int xLeft,xRight;
     int yLeft,yRight;
-    int xMid,yMid;
+    int zLeft,zRight;
+    int xMid,yMid,zMid;
+    SegmentTree3DNode*son[8];
 
+    SegmentTree3DNode(int xL,int xR,int yL,int yR,int zL,int zR) :
+            xLeft(xL),xRight(xR),
+            yLeft(yL),yRight(yR),
+            zLeft(zL),zRight(zR),
+            xMid(xL+xR/2),yMid(yL+yR/2),zMid(zL+zR/2){}
+
+    update(int xL,int xR,int yL,int yR,int zL,int zR)
 };
 
-template <typename T>
+template<typename T>
 class SegmentTree3D{
 public:
     int xLength;
     int yLength;
-    T** data;
-    SegmentTree3D(int xLength,int yLength,T** source){
+    T**data;
+
+    SegmentTree3D(int xLength,int yLength,T**source){
 
     }
 };
 
 
 int main(){
-    std::cout<<"Hello, World!"<<std::endl;
+    cout<<"Hello, World!"<<std::endl;
     return 0;
 }
