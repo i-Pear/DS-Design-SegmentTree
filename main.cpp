988ba6534477457f08b3a6cbf3b0f4b4b15143fd
iPear <i.pear@outlook.com>
2019-12-18 01:10:48 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,14 +27,25 @@ public:
         return xL==xR||yL==yR||zL==zR;
     }
 
+    bool operator == (const Segment3D b)const{
+        return xL==b.xL&&xR==b.xR&&yL==b.yL&&yR==b.yR&&zL==b.zL&&zR==b.zR;
+    }
+
 };
 
-class ReturnType{
+class ExtremeType{
 public:
+    bool valid;
     int min;
     int max;
 
-    ReturnType(int min,int max):min(min),max(max){}
+    ExtremeType(int min,int max): min(min),max(max),valid(true){}
+    ExtremeType(): min(0),max(0),valid(false){}
+    void update(const ExtremeType& b){
+        if(!b.valid)return;
+        min=std::min(min,b.min);
+        max=std::max(max,b.max);
+    }
 };
 
 template<typename T>
@@ -44,8 +55,7 @@ public:
     int xMid,yMid,zMid; // Mid点处算作L范围
     Segment3DTreeNode*son[8];
     int sum;
-    int max;
-    int min;
+    ExtremeType extreme;
     int cachedDiff;
     bool ifSet;
     int cachedSet;
@@ -63,9 +73,9 @@ public:
         cachedDiff=0;
         ifSet=false;
         cachedSet=0;
-        xMid(xL+xR/2);
-        yMid(yL+yR/2);
-        zMid(zL+zR/2);
+        xMid=(xL+xR/2);
+        yMid=(yL+yR/2);
+        zMid=(zL+zR/2);
 
         memset(son,0,sizeof(son));
         // 如果某维度区间大小只有1，则细分该维的L~Mid是无意义的
@@ -82,7 +92,7 @@ public:
             son[5]=new Segment3DTreeNode(xMid,xR,yMid,yR,zL,zMid);
         if(yR-yL>1&&zR-zL>1)
             son[6]=new Segment3DTreeNode(xMid,xR,yL,yMid,zL,zMid);
-        if(xR-xL>1&&yR-yL>1&&zR-zL>!)
+        if(xR-xL>1&&yR-yL>1&&zR-zL>1)
             son[7]=new Segment3DTreeNode(xL,xMid,yL,yMid,zL,zMid);
     }
 
@@ -90,19 +100,21 @@ public:
         Segment3DTreeNode(segment.xL,segment.xR,segment.yL,segment.yR,segment.zL,segment.zR);
     }
 
-    void modifySegment(Segment3D segment,int diff){
-        if(segment.empty())return;
+    ExtremeType modifySegment(Segment3D segment,int diff){
+        if(segment.empty())return {};
         sum+=segment.getArea()*diff;
         if(segment==mySegment){
             cachedDiff+=diff;
-            return;
+            return {};
         }else{
             for(auto &i : son){
-                if(i)
-                    i->modifySegment(segment.intersect(i->mySegment),diff);
+                if(i){
+                    auto re=i->modifySegment(segment.intersect(i->mySegment),diff);
+                    extreme.update(re);
+                }
             }
         }
-
+        
     }
 
     void modifyPoint(int x,int y,int z,int diff){
