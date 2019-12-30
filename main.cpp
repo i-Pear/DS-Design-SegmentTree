0bf15be0ecc3dc625a4fa0da576cb8abf6285b57
iPear <i.pear@outlook.com>
2019-12-31 01:05:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,28 @@
 #include "SegmentPersistenceTree.h"
 
 
-int main(){ // 左闭右开
-    Segment3DPersistenceTree t(10,10,10);
-    t.modifySegment({0,5,0,1,0,1},1);
-    t.modifySegment({3,7,0,2,0,2},10);
-    t.modifySegment({0,5,0,5,0,5},-5);
-    cout<<t.querySegmentMax({0,10,0,10,0,10},3).val;
+/*
+ *             注意左闭右开
+ * */
+
+
+void parse(const string& str){
+
+}
+
+
+int main(){
+    while(true){
+        cout<<"SegmentTree-> ";
+        string input;
+        getline(cin,input);
+        if(input=="exit"){
+            return 0;
+        }
+        try{
+            parse(input);
+        }catch(...){
+            cout<<endl<<"Caught ERROR"<<endl<<endl;
+        }
+    }
 }
